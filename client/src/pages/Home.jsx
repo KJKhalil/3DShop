5bf88e7c8f05060a67e3b9f362b2af57468de8f3
Khalil <khalil.j.khalil2@gmail.com>
2024-05-24 16:42:38 -0400

    added store folder with index.js added CustomButton file to components. Put imports into pages/Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,14 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+import { CustomButton } from '.components';
+import state from '../store';
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+} from '../config/motion';
+
 const Home = () => {
   return (
     <div>Home</div>
